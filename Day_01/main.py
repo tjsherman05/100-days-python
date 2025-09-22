a1b6cfed91f353744b072fb560ce3c63f71eba57
tjsherman05 <tjsherman05@gmail.com>
2025-09-22 12:14:21 -0400

    Day 1 Completed

---
+++ Day_01/main.py
@@ -0,0 +1,5 @@
+# This takes the user input for a city and pet and displays what their band name should be
+print("Welcome! This is a Band Name Generator!")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Generated Band Name: " + city + " " + pet)
