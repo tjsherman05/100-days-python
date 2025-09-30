eaf868119891b16f9c6e1572affbbcf9d7f6ac07
tjsherman05 <tjsherman05@gmail.com>
2025-09-29 22:35:05 -0400

    Day 5 Completed

---
+++ Day_05/README.md
@@ -0,0 +1,22 @@
+# Day 5
+Password Generator Project. Randomly generates a password based on user input for number of letters, symbols, and numbers.
+## 🚀 How to Run
+
+1. Make sure you have [Python 3](https://www.python.org/downloads/) installed.  
+   - On Windows, check with:
+     **py --version**
+     
+   - On Mac/Linux, check with:
+     **python3 --version**
+
+2. Clone this repository:
+   **git clone https://github.com/tjsherman05/100-days-python.git**
+
+3. Navigate to the project folder for this day:
+    **cd 100-days-python/Day_05**
+
+4. Run the Script:
+    - On Windows:
+        **py main.py**
+    - On Mac/Linux:
+        **python3 main.py**
---
+++ Day_05/main.py
@@ -0,0 +1,33 @@
+# Password Generator Project
+# Randomly generate a password based on user input for number of letters, symbols, and numbers.
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+num_letters = int(input("How many letters would you like in your password?\n"))
+num_symbols = int(input(f"How many symbols would you like?\n"))
+num_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_list = []
+
+for letter in range(0, num_letters):
+    password_list.append(random.choice(letters))
+for symbol in range(0, num_symbols):
+    password_list.append(random.choice(symbols))
+for number in range(0, num_numbers):
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your new password is: {password}")
+
+
+
+
